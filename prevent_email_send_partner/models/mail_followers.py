1e05f3ab2a4a143685f1fca2dd2be9e9c740dbef
marcelo <marcelo.pereira@arxi.pt>
2022-04-04 16:40:46 +0100

    fix last commit

--- prevent_email_send_partner/models/mail_followers.py
+++ prevent_email_send_partner/models/mail_followers.py
@@ -6,11 +6,11 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class Followers(models.Model):
+class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
     def _notify_compute_recipients(self, message, msg_vals):
-        recipients_data = super(Followers, self)._notify_compute_recipients(message, msg_vals)
+        recipients_data = super(MailThread, self)._notify_compute_recipients(message, msg_vals)
         _logger.info(f"At _notify_compute_recipients i got the message [{message}] and the vals are [{msg_vals}]")
         _logger.info(f"--------From super i got {recipients_data}")
 
