4419d1b592ed5d65be8d669d9b8ff309bfa19b8d
marcelo <marcelo.pereira@arxi.pt>
2022-04-04 17:02:48 +0100

    update

--- prevent_email_send_partner/models/mail_followers.py
+++ prevent_email_send_partner/models/mail_followers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, Command
+from odoo import models
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -11,10 +11,20 @@ class MailThread(models.AbstractModel):
 
     def _notify_compute_recipients(self, message, msg_vals):
         recipients_data = super(MailThread, self)._notify_compute_recipients(message, msg_vals)
-        _logger.info(f"At _notify_compute_recipients i got the message [{message}] and the vals are [{msg_vals}]")
-        _logger.info(f"--------From super i got {recipients_data}")
-
+        return_list = []
+        if msg_vals.get('author_id'):
+            author_user_id = self.env['res.users'].search([('partner_id', '=', msg_vals['author_id'])])
+            if author_user_id and author_user_id.has_group('base.group_user'):
+                return recipients_data
+            for data in recipients_data:
+                user_id = self.env['res.users'].search([('partner_id', '=', data['id'])])
+                if user_id and user_id.has_group('base.group_user'):
+                    return_list.append(data)
+            return return_list
         return recipients_data
+
+
+
 
 
 
