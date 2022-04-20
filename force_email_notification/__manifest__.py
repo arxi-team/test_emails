1b3e93b1849130268cb949d78de5c92c89877dc0
marcelo <marcelo.pereira@arxi.pt>
2022-04-20 13:01:06 +0100

    [NEW] add force_email_notification

---
+++ force_email_notification/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Force Email Notification",
+    'summary': """Send an email notification for each notification in odoo""",
+    'author': "Arxi",
+    'website': "https://www.arxi.pt",
+    'category': 'Hidden',
+    'version': '15.0.0.0.2',
+    'license': 'OPL-1',
+    'currency': 'EUR',
+    'depends': ['mail'],
+    'data': [
+    ],
+}
